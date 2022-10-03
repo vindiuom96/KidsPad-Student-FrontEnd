d806c0e1c33679ee0f67ac604e90e99fc27b6df8
vindiuom96 <vindipererauom@gmail.com>
2022-10-03 12:29:58 +1100

    Added No question dialog box

---
+++ src/app/page/dialog/no-questions/no-questions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-questions',
+  templateUrl: './no-questions.component.html',
+  styleUrls: ['./no-questions.component.css']
+})
+export class NoQuestionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
